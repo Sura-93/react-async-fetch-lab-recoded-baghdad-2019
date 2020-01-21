704df29463c797897f17eda10b35c257ce2f0ea6
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-21 13:07:37 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+import React, { Component } from 'react';
+class App extends Component {
+    state = {  }
+
+    componentDidMount (){
+
+    
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default App;
